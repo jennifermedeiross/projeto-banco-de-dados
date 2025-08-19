a8430ca4cfc4c9bf116df2c0992d9bea44cff8c5
claraugusta <mc.augusta1052@gmail.com>
2025-08-19 16:25:26 -0300

    adiciona constraint fkclientecliente

--- scripts/TB_CLIENTE.sql
+++ scripts/TB_CLIENTE.sql
@@ -6,5 +6,8 @@ CREATE TABLE cliente (
     sexo                VARCHAR(30),
     email               VARCHAR(100) UNIQUE,
     pontos              INT,
+    cliente_indicador   INT,
     CONSTRAINT pk_cliente PRIMARY KEY(codigo)
+    CONSTRAINT fk_cliente_cliente FOREIGN KEY (cliente_indicador)
+        REFERENCES cliente(codigo)
 );

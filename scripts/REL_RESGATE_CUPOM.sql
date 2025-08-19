9e06cf23104d18bfa0f499980a06df8d78295c20
claraugusta <mc.augusta1052@gmail.com>
2025-08-19 15:55:01 -0300

    remove os delete on cascade

--- scripts/REL_RESGATE_CUPOM.sql
+++ scripts/REL_RESGATE_CUPOM.sql
@@ -5,8 +5,6 @@ CREATE TABLE resgate_cupom (
     CONSTRAINT pk_resgate_cupom PRIMARY KEY (codigo_cliente, codigo_cupom)
     CONSTRAINT fk_resgate_cliente FOREIGN KEY (codigo_cliente)   
         REFERENCES cliente(codigo)
-        ON DELETE CASCADE
     CONSTRAINT fk_resgate_cupom FOREIGN KEY (codigo_cupom)
         REFERENCES CUPOM_DESCONTO(codigo)
-        ON DELETE CASCADE
 );

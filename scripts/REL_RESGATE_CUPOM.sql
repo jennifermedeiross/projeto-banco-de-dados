e6c23019c73664a9f17bceeac78adbf8420cc04a
claraugusta <mc.augusta1052@gmail.com>
2025-08-19 15:50:25 -0300

    pk agora é uma composicao de cod cliente e cod cupom

--- scripts/REL_RESGATE_CUPOM.sql
+++ scripts/REL_RESGATE_CUPOM.sql
@@ -1,9 +1,8 @@
 CREATE TABLE resgate_cupom (
-    codigo          INT,
     codigo_cliente  INT NOT NULL,
     codigo_cupom    INT NOT NULL,
     data_resgate    DATE NOT NULL,
-    CONSTRAINT pk_resgate_cupom PRIMARY KEY (codigo)
+    CONSTRAINT pk_resgate_cupom PRIMARY KEY (codigo_cliente, codigo_cupom)
     CONSTRAINT fk_resgate_cliente FOREIGN KEY (codigo_cliente)   
         REFERENCES cliente(codigo)
         ON DELETE CASCADE

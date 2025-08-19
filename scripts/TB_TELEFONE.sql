a2a267597be3ad960f6582663d896b7c360af558
claraugusta <mc.augusta1052@gmail.com>
2025-08-19 15:29:42 -0300

    refactor: fk_cliente para fk_telefone_cliente

--- scripts/TB_TELEFONE.sql
+++ scripts/TB_TELEFONE.sql
@@ -3,7 +3,7 @@ CREATE TABLE telefone (
     codigo_cliente      INT NOT NULL,
     numero              VARCHAR(20) NOT NULL,
     CONSTRAINT pk_telefone_cliente PRIMARY KEY (codigo),
-    CONSTRAINT fk_cliente FOREIGN KEY (codigo_cliente)
-        REFERENCES cliente(codigo_cliente)
+    CONSTRAINT fk_telefone_cliente FOREIGN KEY (codigo_cliente)
+        REFERENCES cliente(codigo)
         ON DELETE CASCADE
 );

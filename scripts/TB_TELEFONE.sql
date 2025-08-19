f42b9c869ff2e192031a22a54134bf54e8a67da0
claraugusta <mc.augusta1052@gmail.com>
2025-08-19 13:39:41 -0300

    add primary key de telefone e add atributo numero

--- scripts/TB_TELEFONE.sql
+++ scripts/TB_TELEFONE.sql
@@ -1,8 +1,9 @@
 CREATE TABLE telefone (
     telefone_id     INT,
     cliente_id      INT NOT NULL,
-    CONSTRAINT fk_cliente 
-        FOREIGN KEY(cliente_id) 
+    numero          VARCHAR(20) NOT NULL,
+    CONSTRAINT pk_telefone PRIMARY KEY (telefone_id),
+    CONSTRAINT fk_cliente FOREIGN KEY (cliente_id)
         REFERENCES cliente(cliente_id)
         ON DELETE CASCADE
 );

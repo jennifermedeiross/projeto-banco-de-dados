796e40028af98eb3352efcdd82625629f42d05e7
claraugusta <mc.augusta1052@gmail.com>
2025-08-19 15:44:45 -0300

    adiciona constraints de pk e fks

--- scripts/REL_RESGATE_CUPOM.sql
+++ scripts/REL_RESGATE_CUPOM.sql
@@ -1,7 +1,13 @@
 CREATE TABLE resgate_cupom (
+    codigo          INT,
     codigo_cliente  INT NOT NULL,
     codigo_cupom    INT NOT NULL,
     data_resgate    DATE NOT NULL,
-    CONSTRAINT fk_cod_cliente FOREIGN KEY(codigo_cliente)   
+    CONSTRAINT pk_resgate_cupom PRIMARY KEY (codigo)
+    CONSTRAINT fk_resgate_cliente FOREIGN KEY (codigo_cliente)   
         REFERENCES cliente(codigo)
+        ON DELETE CASCADE
+    CONSTRAINT fk_resgate_cupom FOREIGN KEY (codigo_cupom)
+        REFERENCES CUPOM_DESCONTO(codigo)
+        ON DELETE CASCADE
 );

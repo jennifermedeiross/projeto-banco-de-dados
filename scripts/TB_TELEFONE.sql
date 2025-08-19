90622a492b06afbadd5bdd665097e12cf3b598a1
claraugusta <mc.augusta1052@gmail.com>
2025-08-19 13:43:49 -0300

    renomeia pk de telefone

--- scripts/TB_TELEFONE.sql
+++ scripts/TB_TELEFONE.sql
@@ -2,7 +2,7 @@ CREATE TABLE telefone (
     telefone_id     INT,
     cliente_id      INT NOT NULL,
     numero          VARCHAR(20) NOT NULL,
-    CONSTRAINT pk_telefone PRIMARY KEY (telefone_id),
+    CONSTRAINT pk_telefone_cliente PRIMARY KEY (telefone_id),
     CONSTRAINT fk_cliente FOREIGN KEY (cliente_id)
         REFERENCES cliente(cliente_id)
         ON DELETE CASCADE

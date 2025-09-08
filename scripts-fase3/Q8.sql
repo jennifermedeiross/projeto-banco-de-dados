016846ef29db1a850d46531417cc33c72d9d4058
Oscar Rodrigues <ukboscar@gmail.com>
2025-09-08 17:54:31 -0300

    Q8 Done

---
+++ scripts-fase3/Q8.sql
@@ -0,0 +1,5 @@
+SELECT o.cidade, AVG( (SELECT SUM(cpp.quantidade * cpp.valor_atual) FROM compra_possui_produto cpp WHERE cpp.codigo_compra = o.codigo) + o.valor_frete - NVL(o.desconto, 0)) AS valor_medio
+FROM ordem_de_compra o
+GROUP BY o.cidade
+HAVING COUNT(*) > 10
+ORDER BY valor_medio DESC;

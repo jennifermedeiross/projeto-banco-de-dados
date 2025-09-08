c233eb75ea5f606c302b50fd1b95699e453be126
Oscar Rodrigues <ukboscar@gmail.com>
2025-09-08 18:06:57 -0300

    Added: Round

--- scripts-fase3/Q8.sql
+++ scripts-fase3/Q8.sql
@@ -1,4 +1,4 @@
-SELECT o.cidade, AVG( (SELECT SUM(cpp.quantidade * cpp.valor_atual) FROM compra_possui_produto cpp WHERE cpp.codigo_compra = o.codigo) + o.valor_frete - NVL(o.desconto, 0)) AS valor_medio
+SELECT o.cidade, ROUND(AVG( (SELECT SUM(cpp.quantidade * cpp.valor_atual) FROM compra_possui_produto cpp WHERE cpp.codigo_compra = o.codigo) + o.valor_frete - NVL(o.desconto, 0)), 2) AS valor_medio
 FROM ordem_de_compra o
 GROUP BY o.cidade
 HAVING COUNT(*) > 10
